a870d5060733a38ed694edcd78cafd1e932c1f42
austi <austi@Chases-Laptop>
2022-02-08 20:13:49 -0600

    V 4.0 Commit (Partial Completion)

--- src/edu/jsu/mcis/Converter.java
+++ src/edu/jsu/mcis/Converter.java
@@ -73,7 +73,91 @@ public class Converter {
             
             // INSERT YOUR CODE HERE
             
-        }        
+            
+            JSONArray rHeaders = new JSONArray();
+            JSONArray cHeaders = new JSONArray();
+            JSONArray data = new JSONArray();
+            String[] rows;
+
+            /* Get first String[] array from CSV data (the column headers); add elements to "cHeaders" */
+            
+            cHeaders.addAll(Arrays.asList(iterator.next()));
+
+            /* Iterate through remaining CSV rows */
+
+            while (iterator.hasNext()){
+                
+                /* Create container for next row */
+                
+                JSONArray row = new JSONArray();
+                
+                /* Get next String[] array from CSV data */
+                
+                rows = iterator.next();
+                
+                /* Get first element (the row header); add element to "rHeaders" */
+                
+                rHeaders.add(rows[0]);
+                
+                /* Add remaining elements to "data" */
+                
+                for (int i = 1; i < rows.length; i++){
+                    row.add(rows[i]);
+                }
+                
+                /* Add row to "data" */
+                
+                data.add(row);
+
+            }
+
+            /* Construct JSON string (remember, this must be an *exact* match for the example!) */
+            
+            /* Add column and row headers */
+
+            json.append("{\n    \"colHeaders\":").append(cHeaders.toString());
+            json.append(",\n    \"rowHeaders\":").append(rHeaders.toString()).append(",\n");
+            
+            /* Split "data" rows */
+            
+            rows = data.toString().split("],");
+            
+            /* Add data */
+
+            json.append("    \"data\":");
+
+            for (int i = 0; i < rows.length; ++i){
+                
+                String s = rows[i];         /* Get next data row */
+
+                s = s.replace("\"","");     /* Delete double-quotes */
+                s = s.replace("]]","]");    /* Fix terminating square brackets */
+                
+                json.append(s);             /* Append row */
+                
+                 /* If this is not the last data row, close the row and begin a new one */
+                
+                if ((i % rows.length) != (rows.length - 1))
+                    json.append("],\n            ");
+                
+            }
+            
+            /* Close JSON string */
+            
+            json.append("\n    ]\n}");
+            
+            
+            
+            
+        }
+        
+    
+            
+            
+            
+
+
+
         catch(Exception e) { e.printStackTrace(); }
         
         return results.trim();
@@ -92,7 +176,88 @@ public class Converter {
             
             // INSERT YOUR CODE HERE
             
+            
+             /* Create json-simple JSON Parser */
+            
+            JSONParser parser = new JSONParser();
+            
+            /* Parse JSON Data */
+            
+            JSONObject jobject = (JSONObject) parser.parse(jsonString);
+            JSONArray col = (JSONArray) jobject.get("colHeaders");
+            JSONArray row = (JSONArray) jobject.get("rowHeaders");
+            JSONArray data = (JSONArray) jobject.get("data");
+            
+            /* Create String[] arrays for OpenCSV */
+            
+            String[] csvcol = new String[col.size()];
+            String[] csvrow = new String[row.size()];
+            String[] csvdata = new String[data.size()];
+            String[] rowdata;
+            
+            /* Copy column headers */
+
+            for (int i = 0; i < col.size(); i++) {
+                csvcol[i] = col.get(i) + "";
+            }
+            
+            /* Copy row headers and row data */
+
+            for (int i = 0; i < row.size(); i++) {
+                
+                csvrow[i] = row.get(i) + "";
+                csvdata[i] = data.get(i) + "";
+
+            }
+            
+            /* Create OpenCSV Writer */
+
+            
+
+            /* Write column headers */
+
+            csvWriter.writeNext(csvcol);
+
+            /* Write row headers and row data */
+
+            for (int i = 0; i < csvdata.length; i++) {
+                
+                /* Strip square brackets from next row */
+                
+                csvdata[i] = csvdata[i].replace("[","");
+                csvdata[i] = csvdata[i].replace("]","");
+                
+                /* Split csvdata[i] into row elements (using comma as delimiter) */
+
+                String[] elements = csvdata[i].split(",");
+                
+                /* Create String[] container for row data (sized at the number of row elements, plus one for row header) */
+                
+                rowdata = new String[elements.length + 1];
+                
+                /* Copy row header into first element of "rowdata" */
+
+                // INSERT YOUR CODE HERE
+                                rowdata[0] = csvrow[i];
+                
+                /* Copy row elements into remaining elements of "rowdata" */
+                
+                // INSERT YOUR CODE HERE
+                                for(int d = 1; d < csvrow.length; d++){
+                                        rowdata[d] = csvrow[d];
+                                }
+                                
+                
+                /* Write new row */
+                
+                csvWriter.writeNext(rowdata);
+                
+            }
+            
         }
+            
+            
+        
         
         catch(Exception e) { e.printStackTrace(); }
         
---
+++ test/edu/jsu/mcis/JUnitTest.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit3TestClass.java to edit this template
+ */
+package edu.jsu.mcis;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+/**
+ *
+ * @author austi
+ */
+public class JUnitTest extends TestCase {
+    
+    public JUnitTest(String testName) {
+        super(testName);
+    }
+    
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+    
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    // TODO add test methods here. The name must begin with 'test'. For example:
+    // public void testHello() {}
+    
+    
+    public static String testMethod2() {
+    return "Hello there, world!";
+}
+    
+    
+  
+    
+    
+    
+    
+}
